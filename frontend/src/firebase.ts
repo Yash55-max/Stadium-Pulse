d31258a44b126fdd90645ba9d75d7b25f8704c0a
Yash55-max <yashwanthponnam55@gmail.com>
2026-07-19 14:26:46 +0530

    polished

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0ZoxjJ72zIsqivr2wn1_H7M76loAq8kI",
+  authDomain: "studio-1560217422-e282c.firebaseapp.com",
+  projectId: "studio-1560217422-e282c",
+  storageBucket: "studio-1560217422-e282c.firebasestorage.app",
+  messagingSenderId: "871363060347",
+  appId: "1:871363060347:web:08ffcd201ce147060c38de"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
